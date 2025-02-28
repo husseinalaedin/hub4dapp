3dd8d0406a31eb2fc11f73c05e0744653fc09bee
Hussein Alaedin <husseinalaedin@hotmail.com>
2025-02-28 01:46:08 -0500

    main

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { reactRouter } from "@react-router/dev/vite";
- 
+import Inspect from 'vite-plugin-inspect';
+import compression from "vite-plugin-compression";
+
 export default defineConfig({
   base:'/app/',
   plugins: [
     reactRouter(),
-    
+    Inspect(),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz", // Generates .gz files
+      threshold: 1024, // Only compress files larger than 1KB
+      deleteOriginFile: false, // Keep original files
+    }),
   ],
   
   // build: {
